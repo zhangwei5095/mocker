b9c9c35105872468fb1b7616b4ffec5184b58528
franckchen <franckchen@foxmail.com>
2015-11-23 17:00:06 +0000

    更新依赖，准备转型webpack

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+/**
+ * @file webpack配置文件
+ * @author Franck Chen(chenfan02@baidu.com)
+ */
+
+var path = require('path');
+
+module.exports = {
+    // 源文件代码根目录
+    context: path.join(__dirname, '/public/src'),
+    // 需要打包的源文件，注意是不需要.js后缀的
+    entry: './cf',
+    // 输出
+    output: {
+        // 输出路径
+        path: path.join(__dirname, '/public/asset'),
+        // 输出文件名
+        filename: 'bundle.js'
+    },
+    // 依赖配置
+    resolve: {
+        // 依赖根路径
+        root: path.resolve('./node_modules'),
+        /**
+         * 依赖
+         */
+        alias: {
+            jQuery: 'jquery/dist/jquery.min',
+            angular: 'angular/angular.min'
+        }
+    },
+    noParse: [
+        'jQuery',
+        'angular'
+    ]
+};
