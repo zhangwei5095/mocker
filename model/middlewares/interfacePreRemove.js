cb97de42e53742219ca077376649d8dd826ceafb
franckchen <franckchen@foxmail.com>
2016-02-07 06:12:17 +0000

    bug fix

--- model/middlewares/interfacePreRemove.js
+++ model/middlewares/interfacePreRemove.js
@@ -54,6 +54,9 @@ module.exports = function (next) {
                             }
                         );
                 }
+                else {
+                    resolve();
+                }
             });
     });
 
