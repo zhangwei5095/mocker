c0fb4becc1e6191b820a23c6b45821136d2cabda
chenfan02 <franckchen@foxmail.com>
2016-02-22 03:03:34 +0000

    扩充类型查询功能

--- controllers/admin/responseList.js
+++ controllers/admin/responseList.js
@@ -3,7 +3,12 @@
  * @author Franck Chen(chenfan02@baidu.com)
  */
 
-var interfaceModel = require('../../model/interfaceModel');
+var mongoose = require('mongoose');
+
+var db = require('../../lib/db');
+
+var Interface = db.model('interface');
+var ObjectId = mongoose.Types.ObjectId;
 
 module.exports = {
     method: 'get',
@@ -12,30 +17,45 @@ module.exports = {
         var interfaceId = req.query.interfaceId;
 
         // 不能没有interfaceId
-        if (!interfaceId) {
+        if (!ObjectId.isValid(interfaceId)) {
             next({status: 0});
             return;
         }
 
-        // 活接响应数据
-        var promise = interfaceModel.getResponseList(interfaceId);
+        // 默认情况下，获取的是JSON响应列表
+        var type = req.query.type;
+        type = type ? type : 'JSON';
 
-        promise.then(
-            function (data) {
-                res.render('responseList', {
-                    title: '响应总览',
-                    // 首屏数据
-                    initialData: JSON.stringify({
-                        interfaceId: interfaceId,
-                        responses: data.responses,
-                        activeResponseId: data.activeResponseId,
-                        interfaceURL: data.interfaceURL
-                    })
-                });
-            },
-            function () {
-                next({status: 0});
-            }
-        );
+        Interface
+            .findOne({_id: interfaceId})
+            .select('responses url activeResponse')
+            .populate({
+                path: 'responses',
+                // 根据类型查询
+                match: {
+                    type: type
+                },
+                // populate目前激活的响应，需要的字段只有name
+                select: 'name type'
+            })
+            .lean()
+            .exec()
+            .then(
+                function (data) {
+                    res.render('responseList', {
+                        title: '响应总览',
+                        // 首屏数据
+                        initialData: JSON.stringify({
+                            interfaceId: interfaceId,
+                            responses: data.responses,
+                            activeResponseId: data.activeResponse,
+                            interfaceURL: data.url
+                        })
+                    });
+                },
+                function () {
+                    next({status: 0});
+                }
+            );
     }
 };
