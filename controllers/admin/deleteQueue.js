bac0e93ac3820a71ef33abb9e0fcf92d70061280
chenfan02 <franckchen@foxmail.com>
2016-02-22 11:54:07 +0000

    删除接口

---
+++ controllers/admin/deleteQueue.js
@@ -0,0 +1,42 @@
+/**
+ * @file 删除指定的队列接口
+ * @author Franck Chen(chenfan02@baidu.com)
+ */
+
+var mongoose = require('mongoose');
+
+var db = require('../../lib/db');
+var Queue = db.model('queue');
+
+var ObjectId = mongoose.Types.ObjectId;
+
+module.exports = {
+    method: 'post',
+    controller: function (req, res) {
+        var queueId = req.body.queueId;
+
+        if (!ObjectId.isValid(queueId)) {
+            res.json({
+                status: 1,
+                statusInfo: 'id错误'
+            });
+        }
+
+        Queue
+            .findByIdAndRemove(queueId)
+            .exec()
+            .then(
+                function () {
+                    res.json({
+                        status: 0
+                    });
+                },
+                function () {
+                    res.json({
+                        status: 1,
+                        statusInfo: '保存失败'
+                    });
+                }
+            );
+    }
+};
